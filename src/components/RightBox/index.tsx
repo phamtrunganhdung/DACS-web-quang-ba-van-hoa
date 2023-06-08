bf0292a62f4d0c26c1d3359922b11091b413aa72
Anh Dũng <dungpham4320@gmail.com>
2023-06-08 09:50:03 +0700

    add right box location

--- src/components/RightBox/index.tsx
+++ src/components/RightBox/index.tsx
@@ -1,17 +1,55 @@
 import "./index.less";
-
+import { label } from "@/components/Label";
 import React from "react";
+import { history } from "umi";
 export interface IRightBoxContent {
   key: any;
   title: string;
   date: string | null;
-  content: string;
+  content: any;
+  img: any;
 }
 export interface IRightBox {
   header: string;
   key: any;
+  data: IRightBoxContent;
 }
 
-export default function RightBox() {
-  return <div className="right-box-container">RightBox</div>;
+export default function RightBox({ header, key, data }: IRightBox) {
+  return (
+    <div className="right-box-container" key={key}>
+      <div className="right-box-header">
+        <div className="right-box-title">
+          <label.xl>{header || ""}</label.xl>
+        </div>
+      </div>
+      <div className="right-box-content">
+        {data &&
+          data?.length > 0 &&
+          data.map((i: IRightBoxContent) => {
+            return (
+              <div className="r-child-box" key={i?.key}>
+                <img
+                  src={i?.img}
+                  onClick={() => {
+                    history.push(
+                      `/news/location/details-location?id=${i?.key}`
+                    );
+                  }}
+                />
+                <div className="r-info-box">
+                  <label.lg>
+                    <strong>{i?.title}</strong>
+                  </label.lg>
+                  {i?.content && i?.content}
+                  <label.lg>
+                    <i>{i?.date}</i>
+                  </label.lg>
+                </div>
+              </div>
+            );
+          })}
+      </div>
+    </div>
+  );
 }
