d6b61650823876373d5385b4c9e6a4311380a420
Phạm Trung Anh Dũng <73769984+phamtrunganhdung@users.noreply.github.com>
2023-06-18 21:36:17 +0700

    add pic && vid

---
+++ src/pages/news/picAndVid/data.tsx
@@ -0,0 +1,73 @@
+export const pictureData: any = [
+  {
+    id: "cctn",
+    name: "Cồng chiêng Tây Nguyên",
+    listImg: [
+      "https://tuyengiao.vn/Uploads/2021/5/17/29/trainghiemvanhoacongchiengtaynguyqunhungtamhinh2.jpg",
+      "https://tuyengiao.vn/Uploads/2021/5/17/29/trainghiemvanhoacongchiengtaynguyqunhungtamhinh1.jpg",
+      "https://file1.dangcongsan.vn/DATA/0/2019/04/gdcongchieng3-12_30_55_227.png",
+      "https://file1.dangcongsan.vn/DATA/0/2019/04/gdcongchieng4-12_33_22_327.png",
+      "https://photo-cms-vovworld.zadn.vn/w730/uploaded/vovworld/qosrgt/2018_12_01/cong_meys.jpg",
+      "https://baoangiang.com.vn/image/fckeditor/upload/2023/20230223/images/cong-chieng-060620%20(1).jpg",
+      "https://baodaklak.vn/file/fb9e3a03798789de0179a1704dea238e/old-data/dataimages/201811/original/images2711810_c_ng_chi_ng_gia_lai.gif",
+    ],
+  },
+  {
+    id: "ptdtld",
+    name: "Phong trào đấu tranh ở Lâm Đồng",
+    listImg: [
+      "https://www.baotanglamdong.com.vn/images/phocagallery/1-khangchien.jpg",
+      "https://www.baotanglamdong.com.vn/images/phocagallery/2-khangchien.jpg",
+      "https://www.baotanglamdong.com.vn/images/phocagallery/3-khangchien.jpg",
+      "https://www.baotanglamdong.com.vn/images/phocagallery/4-khangchien.jpg",
+      "https://www.baotanglamdong.com.vn/images/phocagallery/5-khangchien.jpg",
+      "https://www.baotanglamdong.com.vn/images/phocagallery/img_6309.jpg",
+    ],
+  },
+  {
+    id: "kchld",
+    name: "Khảo cổ học ở Lâm Đồng",
+    listImg: [
+      "https://www.baotanglamdong.com.vn/images/phocagallery/khaoco_1.jpg",
+      "https://www.baotanglamdong.com.vn/images/phocagallery/khaoco_2.jpg",
+      "https://www.baotanglamdong.com.vn/images/phocagallery/khaoco_3.jpg",
+      "https://www.baotanglamdong.com.vn/images/phocagallery/khaoco_4.jpg",
+      "https://www.baotanglamdong.com.vn/images/phocagallery/khaoco_5.jpg",
+      "https://www.baotanglamdong.com.vn/images/phocagallery/khaoco_6.jpg",
+      "https://www.baotanglamdong.com.vn/images/phocagallery/khaoco_7.jpg",
+    ],
+  },
+  {
+    id: "vhcdt",
+    name: "Văn hóa các dân tộc",
+    listImg: [
+      "https://www.baotanglamdong.com.vn/images/phocagallery/vhdt_4.jpg",
+      "https://www.baotanglamdong.com.vn/images/phocagallery/vhdt_3.jpg",
+      "https://www.baotanglamdong.com.vn/images/phocagallery/vhdt_1.jpg",
+      "https://www.baotanglamdong.com.vn/images/phocagallery/vhdt_5.jpg",
+      "https://www.baotanglamdong.com.vn/images/phocagallery/vhdt_6.jpg",
+      "https://www.baotanglamdong.com.vn/images/phocagallery/vhdt_0.jpg",
+    ],
+  },
+  {
+    id: "ddtn",
+    name: "Đàn đá Tây Nguyên",
+    listImg: [
+      "https://www.baotanglamdong.com.vn/cache/c/3c88df7208cfc56b2ee91cfffed33979.JPG",
+      "http://adammuzic.vn/wp-content/uploads/2018/05/%C4%90%C3%A0n-%C4%90%C3%A1-1-ADAM-Muzic-190518.png",
+      "https://www.baotanglamdong.com.vn/cache/6/66c90c581991572967cbb7cd7adb67eb.JPG",
+      "http://adammuzic.vn/wp-content/uploads/2018/05/%C4%90%C3%A0n-%C4%91%C3%A1-6-ADAM-Muzic-190518.png",
+      "http://adammuzic.vn/wp-content/uploads/2018/05/%C4%90%C3%A0n-%C4%90%C3%A1-3-ADAM-Muzic-190518.jpg",
+    ],
+  },
+  {
+    id: "stgs",
+    name: "Bộ sưu tập gốm sứ",
+    listImg: [
+      "https://www.baotanglamdong.com.vn/images/BO-SUU-TAP/BO-SUU-TAP-GOM-SU/g4.jpg",
+      "https://www.baotanglamdong.com.vn/media/djmediatools/cache/700x450-crop-90-images_bo-suu-tap_bo-suu-tap-gom-su_g2.jpg",
+      "https://www.baotanglamdong.com.vn/media/djmediatools/cache/700x450-crop-90-images_bo-suu-tap_bo-suu-tap-gom-su_g3.jpg",
+      "https://www.baotanglamdong.com.vn/media/djmediatools/cache/700x450-crop-90-images_bo-suu-tap_bo-suu-tap-gom-su_g1.jpg",
+    ],
+  },
+];
--- src/pages/news/picAndVid/index.tsx
+++ src/pages/news/picAndVid/index.tsx
@@ -2,56 +2,68 @@ import React from "react";
 import "./index.less";
 import BodyTitle from "@/components/BodyTitle";
 import { ButtonFloatLeft } from "@/components/Button";
+import PageContent from "@/components/PageContent";
+import { pictureData } from "./data";
 
 export default function PicAndVid() {
   return (
-    <div>
+    <div className="pic-and-vid-container">
       <BodyTitle />
       <ButtonFloatLeft />
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sem nisl,
-      auctor nec nisl a, egestas cursus mi. Vivamus eu dui consequat nisl tempor
-      tempus non sit amet erat. Aenean facilisis ante auctor magna fringilla,
-      vel vestibulum quam vehicula. Proin nec lacus nisl. Vivamus nunc elit,
-      egestas at ligula eget, fringilla venenatis justo. Nulla semper finibus
-      felis eget eleifend. Donec congue metus nec ex luctus aliquet. Aliquam
-      metus metus, porta et faucibus vel, suscipit in nunc. Morbi pharetra massa
-      ac elit posuere sodales. Nullam imperdiet quis neque at suscipit. Aenean
-      dapibus nisi diam, sit amet blandit velit luctus sed. Donec a ipsum non
-      sapien efficitur molestie. Sed iaculis odio sed turpis lobortis tristique.
-      Sed condimentum nisl in volutpat pellentesque. Morbi tincidunt cursus
-      lorem et elementum. Mauris a efficitur quam. Etiam consectetur porttitor
-      euismod. Pellentesque habitant morbi tristique senectus et netus et
-      malesuada fames ac turpis egestas. Maecenas eget porttitor eros. Nunc ex
-      erat, viverra a convallis quis, suscipit eget nisi. Sed rutrum, diam non
-      vehicula lacinia, ipsum erat gravida nisi, eu sollicitudin lacus mauris in
-      enim. Maecenas volutpat, nunc id feugiat dapibus, erat justo semper lorem,
-      vel interdum turpis sapien eget dolor. Praesent id lacus et ipsum dictum
-      iaculis. Phasellus non iaculis diam. Nunc dapibus ipsum et condimentum
-      congue. Morbi tincidunt sagittis libero non malesuada. Aenean eu justo
-      dolor. Vestibulum congue massa at felis feugiat aliquet. Sed sollicitudin
-      dictum risus sed cursus. Aliquam interdum lectus dolor, sed commodo lorem
-      accumsan viverra. In eu nisl eget eros euismod faucibus. Fusce at
-      consectetur mi, in eleifend elit. Vivamus a bibendum lorem. Aliquam eget
-      tellus auctor, facilisis est vel, laoreet lacus. Donec pharetra mauris
-      condimentum pellentesque facilisis. Mauris sit amet mauris eu diam
-      pulvinar pharetra. Pellentesque at pretium elit, vel semper turpis. Sed
-      feugiat nisl elit, ut sodales sapien consectetur eu. Sed maximus augue
-      sem, at lobortis tellus suscipit ut. Ut id odio quis erat maximus
-      facilisis. Vestibulum et accumsan mauris. Cras ac fermentum tortor, id
-      euismod quam. In vel dui eget elit scelerisque lobortis vitae vitae neque.
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi at odio
-      erat. Donec nunc nibh, tempor at arcu nec, sollicitudin porttitor turpis.
-      Sed sed arcu eget orci condimentum efficitur. Aliquam sagittis risus
-      magna, vitae finibus diam luctus et. Nulla dictum justo suscipit pharetra
-      posuere. Aliquam erat volutpat. Nunc sem lectus, luctus a quam viverra,
-      semper iaculis neque. Cras gravida eu sem sed venenatis. Phasellus
-      tristique velit sed ornare tincidunt. Sed vitae lectus congue orci
-      tincidunt volutpat non euismod leo. Aliquam vitae lobortis magna. Donec
-      vitae tincidunt enim. In enim arcu, convallis vel tortor quis, mollis
-      feugiat ante. Pellentesque pharetra, orci non sollicitudin vehicula, elit
-      ipsum lacinia ipsum, id porta erat ante in ipsum. Pellentesque habitant
-      morbi tristique senectus et netus et malesuada fames ac turpis egestas. Ut
-      dignissim nunc lectus, a feugiat elit commodo nec.
+      <PageContent
+        dateSubmitted={null}
+        showDateSubmitted={false}
+        title={null}
+        rightContent={null}
+        content={
+          <div className="pic-and-vid-content">
+            <div className="picture">
+              <div className="title-pic-n-vid">Hình ảnh</div>
+              <div className="content-pic">
+                {pictureData &&
+                  pictureData?.length > 0 &&
+                  pictureData?.map((item: any, index: number) => {
+                    return (
+                      <div className="picture-box" key={index}>
+                        <div className="title-picture-box">{item?.name}</div>
+                        <div className="img-picture-box">
+                          <div className="first-pic">
+                            <img src={item?.listImg[0]} />
+                          </div>
+                          <div className="other-pic">
+                            {item?.listImg?.length > 0 &&
+                              item?.listImg
+                                ?.slice(1, 4)
+                                ?.map((itm: any, idx: number) => {
+                                  return idx === 2 ? (
+                                    <div
+                                      className="img-n-box img-n-more"
+                                      key={idx}
+                                    >
+                                      <img src={itm} />
+                                      <div className="shadow-img-n-box">
+                                        {`+ ${item?.listImg?.length - 3}`}
+                                      </div>
+                                    </div>
+                                  ) : (
+                                    <div className="img-n-box" key={idx}>
+                                      <img src={itm} />
+                                    </div>
+                                  );
+                                })}
+                          </div>
+                        </div>
+                      </div>
+                    );
+                  })}
+              </div>
+            </div>
+            <div className="video">
+              <div className="title-pic-n-vid">Video</div>
+            </div>
+          </div>
+        }
+      />
     </div>
   );
 }
